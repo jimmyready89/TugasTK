5e5d1ebce963c40cb34a32d436702956928976b4
jimmy <jimmy>
2024-05-04 22:07:21 +0700

    Test

---
+++ src/main/java/com/tp2/tk4/Tk4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tp2.tk4;
+
+/**
+ *
+ * @author user
+ */
+public class Tk4 {
+
+    public static void main(String[] args) {
+        app appFream = new app();
+        
+        appFream.setVisible(true);
+    }
+}
